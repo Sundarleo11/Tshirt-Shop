460202892efef693912e934ace7f8289d6fede64
Sundarleo11 <sundarrock1999@gmail.com>
2022-11-17 22:11:30 +0530

    routesandcontrollers

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express=require('express');
 require("dotenv").config();
 const app=express()
 
+//import the all routers
+const home=require('./routes/home');
+
+//configuration all the routes from middleware
+app.use("/api/v1",home);
 
 //export the app js
 module.exports=app

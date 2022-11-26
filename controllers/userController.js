cb95d0e3e7cb24919161080ed028b1a449a452ed
Sundarleo11 <sundarrock1999@gmail.com>
2022-11-26 18:09:56 +0530

    tested_signip and updatedYml

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,46 +1,48 @@
-const User=require('../models/user');
-const Bigpromise=require('../middlewares/bigpromise');
-const customeError=require('../utils/customeError');
+const User = require('../models/user');
+const Bigpromise = require('../middlewares/bigpromise');
+const customeError = require('../utils/customeError');
 const cookieToken = require('../utils/cookieToken');
-const files=require('express-fileupload');
-const cloudinary=require('cloudinary');
+const files = require('express-fileupload');
+const cloudinary = require('cloudinary');
 
 
-exports.signup= Bigpromise(async(req,res,next)=>{
- 
-  let result;
-  // dO read the images 
-  if (req.files) {
-    let file=req.files.photo;
-    console.log("Hi");
-      result = await cloudinary.v2.uploader.upload(file.tempFilePath,{
-      folder:"user",
-      with:150,
-      crop:"scale",
-  
-     });
-     
-   
+exports.signup = Bigpromise(async (req, res, next) => {
+
+  const { email, name, password } = req.body;
+
+
+  if (!req.files) {
+    return next(new customeError("Photo is required for signup", 400));
   }
- 
-  const {email,name,password}=req.body;
- 
   //Modified
-  if(!email || !name || !password){
+  if (!email || !name || !password) {
     //return res.send("email,name and password are required");
     return next(new customeError("email,name  and password are required", 400));
   }
-  console.log("finally there");
-  const user=await User.create({
+ 
+    let file = req.files.photo;
+
+    const result = await cloudinary.v2.uploader.upload(file.tempFilePath, {
+      folder: "user",
+      with: 150,
+      crop: "scale",
+
+    });
+
+
+  
+
+
+  const user = await User.create({
     email,
     name,
     password,
-    photo:{
-      id:result.public_id,
-      secure_url:result.secure_url,
+    photo: {
+      id: result.public_id,
+      secure_url: result.secure_url,
     }
   });
 
-  cookieToken(user,res);
+  cookieToken(user, res);
 
 })

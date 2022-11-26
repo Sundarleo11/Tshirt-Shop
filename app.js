ed3a734d7f77d03eaed2210af26933a813c8034d
Sundarleo11 <sundarrock1999@gmail.com>
2022-11-26 17:14:20 +0530

    image_handler

--- app.js
+++ app.js
@@ -12,18 +12,24 @@ const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+
+
 //reguler middleware 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //cokkies and files from middleware
-app.use(fileupeload(
+app.use(cookieparser());
+app.use(       //corrected the syntex
     fileupeload({
         useTempFiles:true,
-        tempFileDir:"/tmp",
+        tempFileDir:"/tmp/",
     })
-));
-app.use(cookieparser());
+);
+
+
+//template engine
+app.set("view engine", "ejs");
 
 // morgen middleware
 app.use(morgan('tiny'));
@@ -35,6 +41,13 @@ const User = require('./routes/User');
 //routes from middleware
 app.use("/api/v1", home);
 app.use("/api/v1", User);
+
+
+//test the image handler
+
+app.get("/postform", (req, res) => {
+    res.render("signup");
+});
 
 //export the app js
 module.exports = app

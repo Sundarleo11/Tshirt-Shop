c271aee99abe466126c7bc59f69b270784e4c617
Sundarleo11 <sundarrock1999@gmail.com>
2022-11-27 13:42:04 +0530

    forgotpassword_controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,7 @@ const cookieToken = require('../utils/cookieToken');
 const files = require('express-fileupload');
 const cloudinary = require('cloudinary');
 const user = require('../models/user');
+const mailHelper=require('../utils/mailhelp');
 
 
 exports.signup = Bigpromise(async (req, res, next) => {
@@ -20,18 +21,18 @@ exports.signup = Bigpromise(async (req, res, next) => {
     //return res.send("email,name and password are required");
     return next(new customeError("email,name  and password are required", 400));
   }
- 
-    let file = req.files.photo;
 
-    const result = await cloudinary.v2.uploader.upload(file.tempFilePath, {
-      folder: "user",
-      with: 150,
-      crop: "scale",
+  let file = req.files.photo;
 
-    });
+  const result = await cloudinary.v2.uploader.upload(file.tempFilePath, {
+    folder: "user",
+    with: 150,
+    crop: "scale",
+
+  });
 
 
-  
+
 
 
   const user = await User.create({
@@ -48,33 +49,68 @@ exports.signup = Bigpromise(async (req, res, next) => {
 
 })
 
-exports.login=Bigpromise(async(req,res,next)=>{
+exports.login = Bigpromise(async (req, res, next) => {
 
-  const {email, password}=req.body;
-  if(!email || !password){
+  const { email, password } = req.body;
+  if (!email || !password) {
     return next(new customeError("Please provide your email and password", 400));
   }
 
-  const user= await User.findOne({email}).select("+password");
-  const ispassword=await user.isValidatedPassword(password);
+  const user = await User.findOne({ email }).select("+password");
+  const ispassword = await user.isValidatedPassword(password);
 
 
-  if(!user || !ispassword){
+  if (!user || !ispassword) {
     return next(new customeError("Please check your email and password", 400));
   }
 
   cookieToken(user, res);
 })
 
-exports.logout=Bigpromise(async(req,res,next)=>{
-  res.cookie("token",null,{
-    expires:new Date(Date.now()),
-    httpOnly:true,
+exports.logout = Bigpromise(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
   });
 
   return res.status(200).json({
-    success:true,
-    message:"Logout successfully"
+    success: true,
+    message: "Logout successfully"
 
   });
+})
+
+exports.forgotpassword = Bigpromise(async (req, res, next) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return next(new customeError("Email is required for reset the password", 400));
+  }
+
+  const forgottoken = user.getforgotpasswordToken()
+  await user.save({ validateBeforeSave: false });
+  const myurl = `${req.protocol}://${req.get("host")}/password/reset/${forgottoken}`;
+
+  const message = `copy and past this url and hit enter /n/n, ${myurl}`;
+
+  try {
+    await mailHelper({
+      email: user.email,
+      subject: 'Tshort shop',
+      email,
+
+    })
+    return res.status(200).json({
+      success: true,
+      message: "Tshort shop website for password Reset an email"
+    })
+  } catch (error) {
+    user.forgotpassword = undefined,
+      user.forgottoken = undefined,
+      await user.save({ validateBeforeSave: false });
+    return next(new customeError(console.message, 500));
+  }
+
+
 })
--- routes/User.js
+++ routes/User.js
@@ -1,10 +1,11 @@
 const express=require('express');
 const router= express.Router();
 
-const {user, signup,login,logout}=require("../controllers/userController");
+const {user, signup,login,logout,forgotpassword}=require("../controllers/userController");
 
 router.route("/signup").post(signup);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
+router.route("/forgotpassword").post(forgotpassword);
 
 module.exports= router;

c5eade3df29bf45bce6b50f70125dab968db286b
Sundarleo11 <sundarrock1999@gmail.com>
2022-12-01 21:32:01 +0530

    filter

--- utils/Whereclause.js
+++ utils/Whereclause.js
@@ -1,32 +1,49 @@
 //base -product.find()
 //bigQ- url parmas
-//i, q,. global search keyword
-class WhereClause{
-    constructor(base,bigQ){
-        this.base=base;
-        this.bigQ=bigQ
-    }
-search()
-{
-    const searchkeyword=this.bigQ.search ? {
-        name:{
-            $regex:this.bigQ.search,
-            $options:'i'
-        }
-    }:{}
 
-    this.base=this.base.find({ ...searchkeyword})
-    return this;
-}
-    pager(resultperpage){
-        let currentPage=1
-        if(this.bigQ.page){
-            currentPage =this.bigQ.page;
+
+
+//i, q,. global search keyword
+class WhereClause {
+    constructor(base, bigQ) {
+        this.base = base;
+        this.bigQ = bigQ
+    }
+    search() {
+        const searchkeyword = this.bigQ.search ? {
+            name: {
+                $regex: this.bigQ.search,
+                $options: 'i'
+            }
+        } : {}
+
+        this.base = this.base.find({ ...searchkeyword })
+        return this;
+    }
+
+    filter() {
+        const copyQ = { ...this.bigQ };
+        delete copyQ["search"];
+        delete copyQ["limit"];
+        delete copyQ["page"];
+
+        let stringOfCopyQ = JSON.stringify(copyQ);
+        stringOfCopyQ = stringOfCopyQ.replace(/\b(gte|lte|gt|lt)\b/g(m) = `$${m}`);
+
+        const jsonOfCopy = JSON.parse(stringOfCopyQ);
+        this.base = this.base.find(jsonOfCopy)
+    }
+
+
+    pager(resultperpage) {
+        let currentPage = 1
+        if (this.bigQ.page) {
+            currentPage = this.bigQ.page;
         }
         //formula 
-        const skipVal=resultperpage * (currentPage -1);
-        
-        this.base=this.base.limit(resultperpage).skip(skipVal);
+        const skipVal = resultperpage * (currentPage - 1);
+
+        this.base = this.base.limit(resultperpage).skip(skipVal);
         return this;
     }
 

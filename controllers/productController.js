7f448c5da6a3d10d3c471ded7c3973f5df5760da
Sundarleo11 <sundarrock1999@gmail.com>
2022-12-03 19:19:34 +0530

    AdminUpdateOneProduct

--- controllers/productController.js
+++ controllers/productController.js
@@ -57,6 +57,95 @@ exports.addproduct = (async (req, res, next) => {
 
 })
 
+exports.AdminGetAllProduct = (async (req, res, next) => {
+
+  const Products = await Product.find();
+
+  return res.status(200).json({
+    success: true,
+    Products
+  })
+
+
+
+
+})
+
+exports.singleProducts = (async (req, res, next) => {
+
+  const Products = await Product.findById(req.params.id);
+
+  if(!Products){
+    return next(new customeError("Products is not there",401));
+  }
+
+
+  return res.status(200).json({
+    success: true,
+    Products
+  })
+
+
+
+
+})
+
+exports.AdminUpdateOneProduct=(async(req,res,next)=>{
+  
+  const Products = await Product.findById(req.params.id);
+
+  if(!Products){
+    return next(new customeError("Products is not there",401));
+  }
+
+  const ImageArray = [];
+
+  if (!req.files) {
+    return next(new customeError("Photo are required whille add the product", 401));
+  }
+
+  if (req.files) {
+
+    //destory the image  
+    for (let index = 0; index <Products.photo.length; index++) {
+      const resp = await cloudinary.v2.uploader.destroy(Products.photo[index].id);
+      
+    }
+      
+    
+    //update the image 
+    for (let index = 0; index < req.files.photo.length; index++) {
+
+      let result = await cloudinary.v2.uploader.upload(
+        req.files.photo[index].tempFilePath, {
+        folder: "products",
+        with: 150,
+        crop: "scale",
+
+      });
+
+      ImageArray.push({
+        id: result.public_id,
+        secure_url: result.secure_url,
+      });
+
+    }
+  }
+ 
+  req.body.photo = ImageArray;
+ const product= await Product.findByIdAndUpdate(req.params.id, req.body,{
+    new :true,
+    runValidators:true,
+    userFindAndModify:false
+ })
+
+
+  return res.status(200).json({
+    success: true,
+    product
+  })
+})
+
 exports.getAllproduct = (async (req, res, next) => {
   const resultperpage = 6;
 
--- routes/product.js
+++ routes/product.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const { isLoggedIn,customRoles } = require('../middlewares/user');
-const {product,addproduct,getAllproduct}=require('../controllers/productController');
+const {product,addproduct,getAllproduct,AdminGetAllProduct,singleProducts,AdminUpdateOneProduct}=require('../controllers/productController');
 
 
 router.route("/test").get(product);
 
 // client user
+
 router.route("/Products").get(getAllproduct);
+router.route("/Products/:id").get(singleProducts);
 
 // admin user
 router.route("/admin/product/add").post(isLoggedIn, customRoles('Admin') ,addproduct);
+router.route("/admin/Products").get(isLoggedIn, customRoles('Admin') ,AdminGetAllProduct);
+router.route("/admin/Product/:id").put(isLoggedIn, customRoles('Admin') ,AdminUpdateOneProduct);
 
 module.exports=router;

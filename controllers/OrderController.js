d533a61a7e20b0fe4bc6c8e5dc71d13ad21cf045
Sundarleo11 <sundarrock1999@gmail.com>
2022-12-07 17:12:59 +0530

    loggedInuserOrder

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -46,4 +46,21 @@ exports.getOneOrder = bigpromise(async(req, res, next) => {
        order
      });
    });
+
+
+
+   exports.getLoggedInOrder = bigpromise(async(req, res, next) => {
+   
+      const order= await Order.find({users:req.params._id}).exec();
+      
+      if (!order) {
+          return next(new customeError("Order id not found", 401));
+      }
+   
+       res.status(200).json({
+         success: true,
+         order
+       });
+     });
+   
   
--- routes/Order.js
+++ routes/Order.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 const { 
     createOrder,
-    getOneOrder
+    getOneOrder,
+    getLoggedInOrder
 }
 
     = require("../controllers/OrderController");
@@ -11,5 +12,7 @@ const { isLoggedIn,customRoles } = require('../middlewares/user');
 
 router.route("/orders/create").post(isLoggedIn, createOrder);
 router.route("/orders/:id").get(isLoggedIn, getOneOrder);
+//router.route("/orders/myorder").get(isLoggedIn, getLoggedInOrder);
+router.route("/myorder").get(isLoggedIn, getLoggedInOrder);
 
 module.exports = router;
